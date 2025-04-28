c79bab03de41c5fd41c7057eb74bb4f40de36ff6
Nirali9726 <niraliprajapati0076@gmail.com>
2025-04-28 11:03:34 +0530

    Add files via upload

--- digiclock.js
+++ digiclock.js
@@ -1,11 +1,33 @@
+let b;
 function gct(){
     let a=new Date();
-    let b=a.getHours();
+    b=a.getHours();
     let c=a.getMinutes();
     let d=a.getSeconds();
  //document.write(b);
+
     document.getElementById("clock").innerHTML = b + ":" + c + ":" + d;
     setTimeout(gct,1000);
-}
 
+
+}
 gct();
+
+const body=document.body;
+const greeting=document.getElementById('greeting');
+if(b>=5&& b<12){
+    body.style.backgroundColor = "#FFDEAD";
+    greeting.textContent = "Good Morning!";
+  } else if (b >= 12 && b < 17) {
+    body.style.backgroundColor = "#87CEFA"; 
+    greeting.textContent = "Good Afternoon!";
+  } else if (b>= 17 && b < 20) {
+    body.style.backgroundColor = "#FFA07A"; 
+    greeting.textContent = "Good Evening!";
+  } else {
+    body.style.backgroundColor = "#191970"; 
+    greeting.textContent = "Good Night!";
+  }
+
+  setInterval(updateGreeting, 1000); 
+
